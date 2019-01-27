0697af449a3e178d1793356c53e589eba48f1b6b
Sureshkumar K V <41813572+SureshkumarKV@users.noreply.github.com>
2019-01-27 15:41:58 +0530

    Create TVControl2.ino

---
+++ TVControl2.ino
@@ -0,0 +1,40 @@
+#include <IRremote.h>
+
+IRsend irsend;
+unsigned int S_pwr[68]={4600,4350,700,1550,650,1550,650,1600,650,450,650,450,650,450,650,450,700,400,700,1550,650,1550,650,1600,650,450,650,450,650,450,700,450,650,450,650,450,650,1550,700,450,650,450,650,450,650,450,650,450,700,400,650,1600,650,450,650,1550,650,1600,650,1550,650,1550,700,1550,650,1550,650};
+ 
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON    
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      irsend.sendRaw(S_pwr,68,38);
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
+      Serial.println("Motion ended!");
+      irsend.sendRaw(S_pwr,68,38);
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }
+}
